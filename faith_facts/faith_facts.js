6c207f19b622d08cc554531652ec5043c5dbf181
Yo-Dan <yodan.and.the.sixth.character@gmail.com>
2022-08-21 12:50:34 -0400

    added some faith fact json content

--- faith_facts/faith_facts.js
+++ faith_facts/faith_facts.js
@@ -1,3 +1,9 @@
 async function loadPage() {
-    const response = await fetch
+    const response = await fetch('./__data/faith_facts/faith_facts.json');
+    const json_data = await response.json();
+    let category_html = '';
+    for (const category of json_data['faith_fact_categories']) {
+        category_html += `<div class="category-div"><a class="category-link" href="faith_facts/${category['category']}">${category['category_display']}</a></div>`;
+    }
+    document.getElementById('faith-fact-categories').innerHTML = category_html;
 }
