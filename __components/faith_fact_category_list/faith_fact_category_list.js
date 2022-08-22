d787c13e687de5cb9080570273d61d734822c6f9
Yo-Dan <yodan.and.the.sixth.character@gmail.com>
2022-08-21 20:29:54 -0400

    ripple effect to faith fact categories

--- __components/faith_fact_category_list/faith_fact_category_list.js
+++ __components/faith_fact_category_list/faith_fact_category_list.js
@@ -1,3 +1,25 @@
+function createRipple(evt) {
+    const button = evt.currentTarget;
+
+    const circle = document.createElement("span");
+    const diameter = Math.max(button.clientWidth, button.clientHeight);
+    const radius = diameter / 2;
+
+    circle.style.width = circle.style.height = `${diameter}px`;
+    circle.style.left = `${evt.clientX - button.offsetLeft - radius}px`;
+    circle.style.top = `${evt.clientY - button.offsetTop - radius}px`;
+    circle.classList.add("ripple");
+
+    const ripple = button.getElementsByClassName("ripple")[0];
+
+    if (ripple) {
+        ripple.remove();
+    }
+
+    button.appendChild(circle);
+}
+
+
 class CufFaithFactCategoryList extends HTMLElement {
     constructor() {
         super();
@@ -20,6 +42,16 @@ class CufFaithFactCategoryList extends HTMLElement {
             }
         }
         shadow.querySelector('#faith-fact-categories').innerHTML = category_html;
+        for (const category_link of shadow.querySelectorAll('.category-link')) {
+            category_link.addEventListener("mousedown", createRipple);
+        }
+        document.addEventListener("mouseup", this.removeRipples.bind(null, this));
+    }
+
+    removeRipples(source) {
+        for (const ripple of source.shadowRoot.querySelectorAll('.ripple')) {
+            ripple.remove();
+        }
     }
 }
 
