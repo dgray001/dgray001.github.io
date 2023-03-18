0814c4505d80fad29b92f43ab20581f179cd200e
Yo-Dan <yodan.and.the.sixth.character@gmail.com>
2023-03-18 10:33:08 -0500

    v0.5b: Slider dots

--- __components/slider/slider.js
+++ __components/slider/slider.js
@@ -43,57 +43,88 @@ export class CufSlider extends HTMLElement {
       },
     };
 
-    const container = this.shadow.querySelector('.container');
-    const imgs = this.shadow.querySelector('.images');
-    const dots = this.shadow.querySelector('.dots');
-    const texts = this.shadow.querySelector('.texts');
-    if (!container || !imgs || !dots || !texts) {
+    const wrapper = this.shadow.querySelector('.wrapper');
+    if (!wrapper) {
       throw new Error('Missing required elements');
     }
 
-    container.addEventListener('mousemove', () => {
+    wrapper.addEventListener('mousemove', () => {
       this.stop_moving = true;
       clearTimeout(this.last_stop_moving_timeout);
       this.last_stop_moving_timeout = setTimeout(() => {
         this.stop_moving = false;
-      }, 1200);
+      }, 3000);
     });
-    container.addEventListener('click', this.nextImage.bind(this, true));
+    wrapper.addEventListener('click', this.nextImage.bind(this, true));
 
     let i = 0;
     for (const page in data) {
       i++;
+      const container = document.createElement('div');
+      container.classList.add('container');
+      container.id = `img-${i}`;
       const img = document.createElement('img');
       img.src = `/__images/paintings/${data[page]['image']}.jpg`;
-      img.id = `img-${i}`;
+      container.appendChild(img);
+      const text = document.createElement('a');
+      text.href = `/${page}`;
+      text.innerHTML = data[page]['text'];
+      container.appendChild(text);
       if (i == 1) {
         this.current_index = 1;
-        img.setAttribute('style', 'left: 0px');
-        setInterval(this.nextImage.bind(this, false), 3000);
+        container.setAttribute('style', 'left: 0px');
+        setInterval(this.nextImage.bind(this, false), 5000);
       }
-      imgs.appendChild(img);
+      wrapper.appendChild(container);
     }
     this.max_index = i;
+    const dots = document.createElement('div');
+    dots.classList.add('dots');
+    for (let i = 1; i <= this.max_index; i++) {
+      const dot = document.createElement('div');
+      dot.classList.add('dot');
+      dot.id = `dot-${i}`;
+      if (i == 1) {
+        dot.classList.add('current');
+      }
+      dot.addEventListener('click', () => {
+        this.setImage(this.current_index, i);
+      });
+      dots.appendChild(dot);
+    }
+    wrapper.appendChild(dots);
   }
 
   nextImage(force = false) {
     if (!force && (this.max_index < 1 || this.stop_moving)) {
       return;
     }
+    const last_index = this.current_index;
+    const next_index = this.current_index >= this.max_index ? 1 : this.current_index + 1;
+    this.setImage(last_index, next_index);
+  }
+
+  /**
+   * Sets the current image if not currently in a transition
+   * @param {number} last_index 
+   * @param {number} next_index 
+   */
+  setImage(last_index, next_index) {
     if (this.in_transition) {
       return;
     }
 
-    const old_image = this.shadow.querySelector(`#img-${this.current_index}`);
-    this.current_index++;
-    if (this.current_index > this.max_index) {
-      this.current_index = 1;
-    }
-    const new_image = this.shadow.querySelector(`#img-${this.current_index}`);
-    if (!old_image || !new_image) {
+    const old_image = this.shadow.querySelector(`#img-${last_index}`);
+    const old_dot = this.shadow.querySelector(`#dot-${last_index}`);
+    const new_image = this.shadow.querySelector(`#img-${next_index}`);
+    const new_dot = this.shadow.querySelector(`#dot-${next_index}`);
+    if (!old_image || !old_dot || !new_image || !new_dot) {
       throw new Error(`Missing img element: ${this.current_index}`);
     }
+    this.current_index = next_index;
 
+    old_dot.classList.remove('current');
+    new_dot.classList.add('current');
     old_image.setAttribute('style', 'left: -100vw; transition: left 0.5s ease;');
     new_image.setAttribute('style', 'left: 0px; transition: left 0.5s ease;');
     this.in_transition = true;
