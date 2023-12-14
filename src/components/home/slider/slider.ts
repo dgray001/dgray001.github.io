6c9d393a5e9d6e9090966c1fe39b2364081863e2
Yo-Dan <yodan.and.the.sixth.character@gmail.com>
2023-12-13 20:19:54 -0500

    v0.6f: Working slider

--- src/components/home/slider/slider.ts
+++ src/components/home/slider/slider.ts
@@ -4,21 +4,25 @@ import {until} from '../../../scripts/util';
 import html from './slider.html';
 
 import './slider.scss';
-import { CufHeaderHome } from '../header_home/header_home';
 
 export class CufSlider extends CufElement {
   private wrapper: HTMLDivElement;
+  private dots: HTMLDivElement;
 
-  private imgs = new Map<string, HTMLImageElement>();
+  private imgs: HTMLDivElement[] = [];
+  private dot_els: HTMLButtonElement[] = [];
+  private img_map = new Map<string, HTMLImageElement>();
   private last_scale_used = 0;
-  private stop_moving = true;
+  private stop_moving = false;
   private stop_moving_timeout: NodeJS.Timeout|undefined = undefined;
   private current_index = 0;
+  private in_transition = false;
 
   constructor() {
     super();
     this.htmlString = html;
     this.configureElement('wrapper');
+    this.configureElement('dots');
   }
 
   protected override parsedCallback(): void {
@@ -33,8 +37,11 @@ export class CufSlider extends CufElement {
       container.classList.add('container');
       container.id = `img-${i}`;
       const img = document.createElement('img');
+      img.draggable = false;
+      img.alt = this.imageName(picture);
       container.appendChild(img);
-      this.imgs.set(this.imageName(picture), img);
+      this.imgs.push(container);
+      this.img_map.set(this.imageName(picture), img);
       const text = document.createElement('a');
       text.href = `/${picture}`;
       text.innerHTML = this.imageText(picture);
@@ -42,11 +49,21 @@ export class CufSlider extends CufElement {
       if (picture === 'faith_facts') {
         text.classList.add('ff');
       }
-      if (i === this.current_index) {
-        container.setAttribute('style', 'left: 0px');
-      }
       container.appendChild(text);
       this.wrapper.appendChild(container);
+      const dot = document.createElement('button');
+      dot.classList.add('dot');
+      dot.id = `dot-${i}`;
+      dot.tabIndex = 1;
+      dot.addEventListener('click', () => {
+        this.setImage(this.current_index, i);
+      });
+      this.dot_els.push(dot);
+      this.dots.appendChild(dot);
+      if (i === this.current_index) {
+        container.setAttribute('style', 'left: 0px');
+        dot.classList.add('current');
+      }
     }
     this.wrapper.addEventListener('mousemove', () => {
       this.stop_moving = true;
@@ -91,10 +108,6 @@ export class CufSlider extends CufElement {
     }
   }
 
-  private nextImage(force: boolean) {
-    //
-  }
-
   async updateImageSrcs() {
     const available_scales = [10, 13.333, 16.666, 20, 23.333, 26.666];
     const header = document.getElementsByTagName('cuf-header-home')[0];
@@ -114,11 +127,36 @@ export class CufSlider extends CufElement {
       const scale_invert = 10 / scale;
       this.wrapper.style.setProperty('--scale', scale_invert.toString());
       const display_scale = Math.floor(scale);
-      for (const [painting_name, img] of this.imgs.entries()) {
+      for (const [painting_name, img] of this.img_map.entries()) {
         img.src = `/images/paintings/slider/${painting_name}_${display_scale}.jpg`;
       }
       break;
     }
+  }
+
+  private nextImage(force: boolean) {
+    if (!force && (this.imgs.length < 1 || this.stop_moving)) {
+      return;
+    }
+    const last_index = this.current_index;
+    const next_index = (this.current_index + 1) >= this.imgs.length ? 0 : this.current_index + 1;
+    this.setImage(last_index, next_index);
+  }
+
+  private setImage(last_index: number, next_index: number) {
+    if (this.in_transition) {
+      return;
+    }
+    this.current_index = next_index;
+    this.dot_els[last_index].classList.remove('current');
+    this.dot_els[next_index].classList.add('current');
+    this.imgs[last_index].setAttribute('style', 'left: -100vw; transition: left 0.5s ease;');
+    this.imgs[next_index].setAttribute('style', 'left: 0px; transition: left 0.5s ease;');
+    this.in_transition = true;
+    setTimeout(() => {
+      this.imgs[last_index].removeAttribute('style');
+      this.in_transition = false;
+    }, 500);
   }
 }
 
